813dc5f9a1fb8e16a8f0c70826d3391f481d2710
os250082 <oz.shpigel@ncr.com>
2022-02-16 01:41:15 +0200

    a

---
+++ ConsoleApp3/ActionInputs.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace ConsoleApp3
+{
+    public class ActionInputs
+    {
+        public ActionInputs()
+        {
+        
+        }
+
+        [Option('o', "owner",
+            Required = true,
+            HelpText = "The owner, for example: \"dotnet\". Assign from `github.repository_owner`.")]
+        public string Owner { get; set; } = null!;
+    }
+}
