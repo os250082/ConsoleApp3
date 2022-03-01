3593277ccba6f4b12b6d843efa213cb3323d54cb
os250082 <oz.shpigel@ncr.com>
2022-03-01 19:12:51 +0200

    a

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -8,7 +8,7 @@ Console.WriteLine("Hello, World!");
 static async Task Start(ActionInputs inputs)
 {
     var greetings = Environment.GetEnvironmentVariable("GREETINGS");
-    Console.WriteLine(greetings[0]);
+    Console.WriteLine(greetings);
 }
 
 var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
