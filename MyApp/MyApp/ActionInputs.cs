5d4755ba5d3a76a7837f1d55f15a36f34ec68b01
os250082 <oz.shpigel@ncr.com>
2022-03-01 20:05:33 +0200

    a

--- MyApp/MyApp/ActionInputs.cs
+++ MyApp/MyApp/ActionInputs.cs
@@ -5,11 +5,6 @@ namespace DotNet.GitHubAction
 {
     public class ActionInputs
     {
-        public ActionInputs()
-        {
-            
-        }
-
         [Option('o', "owner", Required = true)]
         public string Owner { get; set; } = null!;
     }
