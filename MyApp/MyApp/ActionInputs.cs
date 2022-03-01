b99beb29ad58ff0691da4619b4ae3389ba08c4d5
os250082 <oz.shpigel@ncr.com>
2022-03-01 15:50:09 +0200

    a

---
+++ MyApp/MyApp/ActionInputs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CommandLine;
+
+namespace DotNet.GitHubAction
+{
+    public class ActionInputs
+    {
+        string _repositoryName = null!;
+        string _branchName = null!;
+
+        public ActionInputs()
+        {
+            var greetings = Environment.GetEnvironmentVariable("GREETINGS");
+            if (greetings is { Length: > 0 })
+            {
+                Console.WriteLine(greetings);
+                Console.WriteLine(" aaa");
+            }
+        }
+
+        [Option('o', "owner",
+            Required = true,
+            HelpText = "The owner, for example: \"dotnet\". Assign from `github.repository_owner`.")]
+        public string Owner { get; set; } = null!;
+    }
+}
---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+using DotNet.GitHubAction;
+using static CommandLine.Parser;
+
+Console.WriteLine("Hello, World!");
+
+static async Task Start(ActionInputs inputs)
+{
+
+}
+
+var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
+await parser.WithParsedAsync(options => Start(options));
