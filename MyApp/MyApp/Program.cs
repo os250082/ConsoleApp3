5991caac7c959d61c9615cd345686793ca948b35
os250082 <oz.shpigel@ncr.com>
2022-03-02 10:58:47 +0200

    aa

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -8,6 +8,15 @@ Console.WriteLine("Hello, World!");
 static async Task Start(ActionInputs inputs)
 {
     Console.WriteLine(inputs.Owner);
+
+    string testDll = @"C:\GitHub\r10-server\Servers\Store\App\LibsLocal\Retalix.StoreServer.Model.Test.dll";
+    var assenmblyLoader = new TestAssemblyLoader(testDll);
+    var testsClasses = assenmblyLoader
+    .GetTestsClasses()
+    .OrderBy(x => x.Name)
+    .ToList();
+
+    Console.WriteLine(testsClasses);
 }
 
 var parser = Default.ParseArguments<ActionInputs>(() => new(), args);
---
+++ MyApp/MyApp/TestAssemblyLoader.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DotNet.GitHubAction
+{
+    public class TestAssemblyLoader
+    {
+        Assembly AssemblyToLoad;
+        public TestAssemblyLoader(string assemblyToLoad)
+        {
+            AssemblyToLoad = Assembly.LoadFrom(assemblyToLoad);
+        }
+       
+        public IEnumerable<Type> GetTestsClasses()
+        {
+            return AssemblyToLoad.GetTypes()
+                .Where(type => type.GetCustomAttributes
+                (typeof(TestClassAttribute), true).Length > 0);
+        }
+
+        public string GetAssemblyName()
+        {
+            return AssemblyToLoad.ManifestModule.Name;
+        }
+    }
+}
